cd5d3c2f5d64e23874232bdf87e94a0a8f4ca37c
Bruce Robertson <brobertson@mta.ca>
2013-04-17 09:02:02 -0300

    include database info

--- settings.py
+++ settings.py
@@ -1,6 +1,8 @@
 #settings.py
 import os
-# __file__ refers to the file settings.py 
+# __file__ refers to the file settings.py
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))   # refers to application_top
 APP_STATIC = os.path.join(APP_ROOT, 'static')
-POSSIBLE_HOCR_VIEWS = ['selected_hocr_output_spellchecked','combined_hocr_output', 'selected_hocr_output']
+POSSIBLE_HOCR_VIEWS = ['combined_hocr_output','selected_hocr_output_spellchecked','selected_hocr_output']
+SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test2.db'#'sqlite:///' + os.path.join(APP_ROOT, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(APP_ROOT, 'db_repository')
