6e1e7246599233539f549cdd160ac049ecc90b64
Bruce Robertson <brobertson@mta.ca>
2013-04-17 09:01:34 -0300

    main code changed to work toward database

--- lace.py
+++ lace.py
@@ -1,7 +1,46 @@
 from flask import Flask, url_for
+from flask.ext.sqlalchemy import SQLAlchemy
 from settings import APP_ROOT, POSSIBLE_HOCR_VIEWS
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/2test.db'
+db = SQLAlchemy(app)
 
+#db models
+
+class Archivetext(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    archive_id = db.Column(db.String(64), index = True, unique = True)
+    creator = db.Column(db.String(120), index = True, unique = False)
+    publisher = db.Column(db.String(120), index = True, unique = False)
+    date = db.Column(db.String(120), index = True, unique = False)
+    title = db.Column(db.String(120), index = True, unique = False)
+    volume = db.Column(db.String(120), index = True, unique = False)
+    ppi = db.Column(db.String(120), index = True, unique = False)
+    def __repr__(self):
+        return '<Archive_id %r>' % (self.archive_id)
+
+class Ocrrun(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    archivetext_id = db.Column(db.Integer, db.ForeignKey('archivetext.id'))
+    classifier = db.Column(db.String(64), index = True, unique = False)
+    date = db.Column(db.String(120), index=True, unique = False)
+    archivetext = db.relationship('Archivetext',
+                                       backref=db.backref('ocrruns', lazy='dynamic'))#b_score = db.Column(db.Float, index = True, unique = False)
+
+    def __repr__(self):
+         return '<Ocrrun %r>' % (self.archivetext + self.date)
+
+class Outputpage(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    b_score  =  db.Column(db.Float, index = True, unique = False)
+    output_type = db.Column(db.Integer, index = True, unique = False)
+    page_number = db.Column(db.Integer, index = True, unique = False)
+    threshold = db.Column(db.Integer, index = True, unique = False)
+    content = db.Column(db.String, index = False, unique = False)
+    ocrrun_id = db.Column(db.Integer, db.ForeignKey('ocrrun.id'))
+    ocrrun = db.relationship('Ocrrun', backref=db.backref('outputpages', lazy='dynamic'))
+    def __repr__(self):
+        return '<Outputpage ' + str(ocrrun) + " " + str(output_type) + " " + str(page_number) + ">"
 
 @app.route('/')
 def index():
@@ -9,12 +48,17 @@ def index():
     return render_template('basic.html', name="foo")
 
 
+@app.route('/about')
+def about():
+    return index()
+
+
 def pad_page_num_for_archive(num):
     return num.zfill(4)
 
 
 def url_for_page_image(text_id, page_num):
-    file_address = 'Images/Color/' + text_id + \
+    file_address = 'Images/' + text_id + \
         '_color/' + text_id + '_' + page_num + '.jpg'
     return url_for('static', filename=file_address)
 
@@ -62,8 +106,12 @@ def side_by_side_view(html_path):
             os.path.dirname(html_path), sp[this_page_index + 1]))
     else:
         page_after = None
-    (text_id, page_num) = filename.split('_')
-    page_num = page_num[0:-5]
+    try:
+        (text_id, page_num) = filename.split('_')
+        page_num = page_num[0:-5]
+    except ValueError:
+        (long_text_id, page_num, file_type, thresh, threshvalue) = filename.split('_')
+        text_id = long_text_id[7:]
     (view, date, classifier) = parse_path(html_path)
     pagination_array = make_pagination_array(
         number_of_sister_pages, this_page_index, 25, html_path, sp)
@@ -111,8 +159,11 @@ def parse_path(textpath):
     import os.path
     last_dir = os.path.basename(os.path.dirname(textpath))
     # last_dir = os.path.dirname(textpath)
+    return parse_dir(last_dir)
+
+def parse_dir(textdir):
     for view in POSSIBLE_HOCR_VIEWS:
-        (before, sep, after) = last_dir.partition('_' + view)
+        (before, sep, after) = textdir.partition('_' + view)
         print
         print before, sep, after
         if sep:
@@ -121,11 +172,12 @@ def parse_path(textpath):
             classifier = '_'.join(parts[1:])
             return (view, date, classifier)
     raise ValueError(
-        "Path " + last_dir + " cannot be parsed with given views.")
+        "Path " + textdir + " cannot be parsed with given views.")
+
 
 
 def get_absolute_textpath(textpath):
-    return APP_ROOT + url_for('static', filename="Texts/" + textpath)
+    return APP_ROOT + '/static/Texts/' + textpath #url_for('static', filename="Texts/" + textpath)
 
 
 def get_sister_pages(textpath):
@@ -185,7 +237,19 @@ def get_text_info(textid):
     info = collect_archive_text_info(metadata_file)
     return info
 
-
+@app.route('/runinfo', methods=['GET'])
+def runinfo():
+    from flask import request, render_template
+    text_id = request.args.get('text_id', '')
+    date = request.args.get('date', '')
+    classifier = request.args.get('classifier', '')
+    path_to_info = text_id + '/' + date + '_' + classifier + '_selected_hocr_output/info.txt'
+    path_to_3d =  url_for('static', filename='Texts/' + text_id + '/' + date + '_' + classifier + '_selected_hocr_output/3d.png')
+    path_to_chart = url_for('static', filename='Texts/' + text_id + '/' + date + '_' + classifier + '_selected_hocr_output/' + date  + '_' + classifier + '_summary.png')
+    info_file = open(get_absolute_textpath(path_to_info))
+    info = info_file.read()
+    text_info = get_text_info(text_id)
+    return render_template('run_info.html', text_id = text_id, date = date, classifier = classifier, text_info= text_info, info=info, path_to_3d = path_to_3d, path_to_chart = path_to_chart)
 
 # Takes HOCR page and injects stylesheet call so that
 # greek is handled by webfont GFS-Porson
@@ -200,7 +264,7 @@ def collect_archive_text_info(archive_xml_file):
     # returns dictionary with appropriate single-value strings
     info = {}
     categories = ['creator', 'title', 'ppi',
-                  'publisher', 'date', 'identifier-access', 'volume']
+                  'publisher', 'date', 'identifier', 'volume']
     tree = etree.parse(archive_xml_file)
     for category in categories:
         data = tree.xpath('/metadata/' + category + '/text()')
@@ -209,8 +273,83 @@ def collect_archive_text_info(archive_xml_file):
             if len(data) > 1:
                 info[category] = info[category] + ' et al.'
         except IndexError:
-            info[category] = None
+            info[category] =''
     return info
 
+@app.route('/catalog')
+def catalog():
+    from flask import render_template
+    import os
+    works = []
+    text_dirs = os.listdir(os.path.dirname(get_absolute_textpath('')))
+    for text_dir in text_dirs:
+        text_id = os.path.basename(text_dir)
+        try:
+            text_info = get_text_info(text_id)
+            link = url_for('runs', text_id = text_id)
+            print link
+            text_info['link'] = link
+            works.append(text_info)
+        except:
+            pass
+    sorted_works = sorted(works, key=lambda work: work['creator'])
+    return render_template('catalog.html', works = sorted_works)
+
+
+def get_runs(text_id):
+    import os
+    import glob
+    path_to_runs = get_absolute_textpath(text_id)
+    run_dirs = os.listdir(path_to_runs)
+    run_dirs = [elem for elem in run_dirs if 'output' in elem ]
+    run_dates = sorted(set([elem[0:16] for elem in run_dirs]))# gets uniq dates
+    print run_dates
+    run_info = []
+    for run_date in run_dates:
+        run = {}
+        run_details = {}
+        run['date'] = run_date
+        dir_for_glob =  path_to_runs + '/' + run_date
+        sel_hocr_dir_glob = dir_for_glob + '*selected_hocr_output'
+        sel_hocr_dir = glob.glob(sel_hocr_dir_glob)[0]
+        try:
+            score_file = open(sel_hocr_dir + '/best_scores_sum.txt')
+            run['score'] = score_file.read()
+        except IOError:
+            run['score'] = 0
+        (a,b,classifier) = parse_dir(sel_hocr_dir)
+        run['classifier'] = classifier
+        output_dirs = possible_output_dirs(dir_for_glob)
+        sel_hocr_dir_output = glob.glob(sel_hocr_dir + '/*html')
+        #but this is a filesystem directory, not a URL. We need to remove the path to 'static'
+        #first_output = first_output[len(APP_ROOT + '/static/Texts/'):]
+        run['link'] = ''#url_for('side_by_side_view', html_path = first_output)
+        if len(sel_hocr_dir_output) > 0:
+            run_info.append(run)
+        else:
+            print "there's nothing in", sel_hocr_dir
+        print run_info
+    sorted_run_info = sorted(run_info, reverse=True, key=lambda run: float(run['score']))
+    return (dir_for_glob, sorted_run_info)
+
+@app.route('/runs/<text_id>')
+def runs(text_id):
+    from flask import render_template
+    (text_info, sorted_run_info) = get_runs(text_id)
+    return render_template('runs.html', text_info = text_info, runs = sorted_run_info)
+
+def possible_output_dirs(prefix):
+    import os
+    import glob
+    output = []
+    for view in POSSIBLE_HOCR_VIEWS:
+        try:
+            one_dir = glob.glob(prefix + '*' + view)[0]
+            output.append(one_dir)
+        except:
+            pass
+    return output
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
+
