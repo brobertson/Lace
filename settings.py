32fa929f343ff7f92d8f93c1662a61aa52ab33c6
Bruce Robertson <brobertson@mta.ca>
2013-05-15 17:21:27 -0300

    include blended views

--- settings.py
+++ settings.py
@@ -3,7 +3,8 @@ import os
 # __file__ refers to the file settings.py
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))   # refers to application_top
 APP_STATIC = os.path.join(APP_ROOT, 'static')
-POSSIBLE_HOCR_VIEWS = ['raw_hocr_output','combined_hocr_output','selected_hocr_output_spellchecked','selected_hocr_output']
-PREFERENCE_OF_HOCR_VIEWS=['combined_hocr_output','selected_hocr_output_spellchecked','selected_hocr_output','raw_hocr_output']
+#only APPEND to this list in order to ensure backward compatibility
+POSSIBLE_HOCR_VIEWS = ['raw_hocr_output','combined_hocr_output','selected_hocr_output_spellchecked','selected_hocr_output','blended_hocr_output']
+PREFERENCE_OF_HOCR_VIEWS=['combined_hocr_output','blended_hocr_output','selected_hocr_output_spellchecked','selected_hocr_output','raw_hocr_output']
 SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test2.db'#'sqlite:///' + os.path.join(APP_ROOT, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(APP_ROOT, 'db_repository')
