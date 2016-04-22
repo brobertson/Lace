43f792506c793d3a3507838b5dcf2d34fe1b8a5c
Bruce Robertson <brobertson@mta.ca>
2016-04-22 10:53:46 -0300

    ant task to make a new user

---
+++ eXist-db/ant/sample.sh
@@ -0,0 +1,1 @@
+sudo ant -f adduser.xml -Dpath=/usr/local/eXist-db -Dlogin=brucefoo -Dsecret=brucefoo -Duser.group=bruceroo -Droot.login=admin -Droot.password=proxen*s -Dexist.uri=http://localhost:8080/
