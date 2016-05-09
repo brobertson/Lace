29ff4f0a62a89ead48086c6ddddec181ac9471c9
Bruce Robertson <brobertson@mta.ca>
2016-05-09 14:24:33 -0300

    add authentication

--- example_local_settings.py
+++ example_local_settings.py
@@ -3,3 +3,4 @@ database_uri = 'sqlite:////tmp/lace_sqlite.db'
 #database_uri = "mysql://brucerob@127.0.0.1:3306/secretlace"
 #database_uri = 'sqlite://' + textpath_root + 'secret_lace.db'
 exist_db_uri = "http://localhost:8080/exist/"
+http_auth_secret_key = "zvjhIvgvi8"
